c79627944529b750633eae27bc40dd7aca74d319
Lazos <43473533+LazosPap@users.noreply.github.com>
2023-10-19 17:41:51 +0300

    Projects Page Complete

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,25 @@
+import projectimg from '../img/React Music Player Thumbnail.png'
+
 const Projects = () => {
     return (
-        <div>
-            <h1>Projects</h1>
+<div className="projects-main">
+    <div class="cardBox">
+    <div class="card">
+        <h2>React Music Player</h2>
+        <div class="content">
+        <a href="https://lazospap.github.io/reactplayer/" add a target="_blank"><img src={projectimg}></img></a>
         </div>
+    </div>
+    </div>
+    <div class="cardBox">
+    <div class="card">
+        <h2>Coming Soon</h2>
+        <div class="content">
+        <h3>Nothing to check here</h3>
+        </div>
+    </div>
+    </div>
+</div>
     );
 };
 
